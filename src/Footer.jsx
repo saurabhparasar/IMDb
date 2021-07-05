3e095a04c76e89c274c89534e3255242e4cdc69f
saurabh parasar <saurabhparasar3010@gmail.com>
2021-07-06 00:54:58 +0530

    copyright with date function

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,54 +1,64 @@
 import React from "react";
 import "./Footer.css";
 function Footer() {
-    return(
-        <div className="main-footer">
-            <div className="container">
-                <div className="row">
-                    {/* Column1 */}
-                    <div className="col-md-3 col-sm-6">
-                       <ul className="list-unstyled">
-                           <li>Get the Imdb App</li>
-                           <li>Press Room</li>
-                       </ul>
-                   </div>
-                   {/* Column 2 */}
-                   <div className="col-md-3col-sm-6">
-                   <ul className="list-unstyled">
-                           <li>Help</li>
-                           <li>Advertising</li>
-                           </ul>
+  return (
+    <div className="main-footer">
+      <div className="container">
+        <div className="row">
+          {/* Column1 */}
+          <div className="col-md-3 col-sm-6">
+            <ul className="list-unstyled">
+              <li>Get the Imdb App</li>
+              <li>Press Room</li>
+            </ul>
+          </div>
+          {/* Column 2 */}
+          <div className="col-md-3col-sm-6">
+            <ul className="list-unstyled">
+              <li>Help</li>
+              <li>Advertising</li>
+            </ul>
+          </div>
+          {/* Column 3 */}
+          <div className="col-md-3col-sm-6">
+            <ul className="list-unstyled">
+              <li>Site Index</li>
+              <li>Jobs</li>
+            </ul>
+          </div>
+          {/* Column 4 */}
+          <div className="col-md-3col-sm-6">
+            <ul className="list-unstyled">
+              <li>IMDbPro</li>
+              <li>Condition of Use</li>
+            </ul>
+          </div>
+          {/* Column 5 */}
+          <div className="col-md-3col-sm-6">
+            <ul className="list-unstyled">
+              <li>Box Office Mojo</li>
+              <li>Privacy Policy</li>
+            </ul>
+          </div>
+          {/* Column 6 */}
+          <div className="col-md-3col-sm-6">
+            <ul className="list-unstyled">
+              <li>IMDb Developer</li>
+              <li>Intrested-Based Ads</li>
+            </ul>
+          </div>
 
-                   </div>
-                    {/* Column 3 */}
-                    <div className="col-md-3col-sm-6">
-                   <ul className="list-unstyled">
-                           <li>Site Index</li>
-                           <li>Jobs</li>
-                           </ul>
+          <div className="bottom-text">
+            <p>
+              {" "}
+              an Amazon Company &copy;{new Date().getFullYear()} by IMDb.com,
+              Inc.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
 
-                   </div>
-                     {/* Column 4 */}
-                     <div className="col-md-3col-sm-6">
-                   <ul className="list-unstyled">
-                           <li>IMDbPro</li>
-                           <li>Condition of Use</li>
-                           </ul>
-
-                   </div>
-                     {/* Column 5 */}
-                     <div className="col-md-3col-sm-6">
-                   <ul className="list-unstyled">
-                           <li>Box Office Mojo</li>
-                           <li>Privacy Policy</li>
-                           </ul>
-
-                   </div>
-                     {/* Column 6 */}
-                     <div className="col-md-3col-sm-6">
-                   <ul className="list-unstyled">
-                           <li>IMDb Developer</li>
-                           <li>Intrested-Based Ads</li>
-                           </ul>
-                 </div>
-
+export default Footer;
