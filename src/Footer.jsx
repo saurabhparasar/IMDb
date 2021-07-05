98f70a0405245640e7a4307368f488e56067d7ed
saurabh parasar <saurabhparasar3010@gmail.com>
2021-07-06 00:51:32 +0530

    functions

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,2 +1,8 @@
 import React from "react";
 import "./Footer.css";
+function Footer() {
+    return(
+        <div className="main-footer">
+            <div className="container">
+                <div className="row">
+ 
