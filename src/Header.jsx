e40f6c41d94874b86b7e2c281399c031a013aa5a
saurabh parasar <saurabhparasar3010@gmail.com>
2021-07-05 23:42:03 +0530

    nav bar

--- src/Header.jsx
+++ src/Header.jsx
@@ -2,3 +2,14 @@ import React from "react";
 import SearchIcon from "@material-ui/icons/Search";
 import "./Header.css";
 import { Link } from "react-router-dom";
+function Header() {
+
+    
+    return (
+      <nav className="header">
+          <img className="header__logo" src="" alt="logo " src="https://m.media-amazon.com/images/G/01/IMDb/BG_rectangle._CB1509060989_SY230_SX307_AL_.png"></img>
+          <div className="header__Menu"> Menu </div>
+          <div className="header__search">
+              <input type="text" className="header__searchinput"  />
+              <SearchIcon className="header__searchIcon" />
+          </div>
