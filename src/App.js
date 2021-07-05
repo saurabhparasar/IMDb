07d53499fe28995e5476e1eca2c44e0dd368a1c5
saurabh parasar <saurabhparasar3010@gmail.com>
2021-07-05 23:28:50 +0530

    imported modules

--- src/App.js
+++ src/App.js
@@ -1,25 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+import "./App.css";
+import Header from "./Header";
+import Footer from "./Footer";
+import Login from "./Login";
+import Watchlist from "./Watchlist";
+import Movies from "./Movies";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./Home";
