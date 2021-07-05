b6f940efcf5e35fba72c81e973d86e1d6fbc6bd4
saurabh parasar <saurabhparasar3010@gmail.com>
2021-07-05 23:55:36 +0530

    return statement

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,23 +1,32 @@
 import React, { useState, useEffect } from "react";
 import Movies from "./Movies";
 import "./Home.css";
-function Home()  {
-    <div>
-    <div className="home"/>
-    <div className="home__image"/>
-    <div className="home__row"/>
+function Home() {
+  <div>
+    <div className="home" />
+    <div className="home__image" />
+    <div className="home__row" />
+  </div>;
+  const [movies, setMovies] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
+
+  useEffect(async () => {
+    let response = await fetch(
+      "http://www.omdbapi.com/?s=action&apikey=db9a8242"
+    );
+    let responseJson = await response.json();
+
+    if (responseJson.Search) {
+      setMovies(responseJson.Search);
+    }
+  }, []);
+  return (
+    <div className="home">
+      {movies.map((item) => (
+        <img src={item.Poster} />
+      ))}
     </div>
-	const [movies, setMovies] = useState([]);
-	const [searchValue, setSearchValue] = useState('');
+  );
+}
 
-    useEffect( async () =>  {
-		
-
-		let response = await fetch('http://www.omdbapi.com/?s=action&apikey=db9a8242');
-		let responseJson = await response.json();
-
-		if (responseJson.Search) {
-			setMovies(responseJson.Search);
-		};
-    },[])
-
+export default Home;
