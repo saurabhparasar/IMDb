26603a68e9ba521c8929e5a7efbb22d566c4f433
saurabh parasar <saurabhparasar3010@gmail.com>
2021-07-05 23:53:56 +0530

    spread operators

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,3 +1,12 @@
 import React, { useState, useEffect } from "react";
 import Movies from "./Movies";
 import "./Home.css";
+function Home()  {
+    <div>
+    <div className="home"/>
+    <div className="home__image"/>
+    <div className="home__row"/>
+    </div>
+	const [movies, setMovies] = useState([]);
+	const [searchValue, setSearchValue] = useState('');
+
